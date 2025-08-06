fe36b38b5971126adf138a336d54f96576e05b5c
Giulio-Marra <giuliomarramail@gmail.com>
2025-08-06 10:49:39 +0200

    Inizializzazione progetto Spring Boot con dipendenze e configurazione PostgreSQL

---
+++ backend/src/test/java/giuliomarra/bookhaven/BookhavenApplicationTests.java
@@ -0,0 +1,13 @@
+package giuliomarra.bookhaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookhavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
