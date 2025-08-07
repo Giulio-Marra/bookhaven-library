4946a89b2a2c7c98d5fc79a9d516aeeaee15fdef
Giulio-Marra <giuliomarramail@gmail.com>
2025-08-07 11:56:08 +0200

    (Backend) Gestione filtri per la autorizzazioni in base al ruolo

--- backend/src/main/java/giuliomarra/bookhaven/security/Config.java
+++ backend/src/main/java/giuliomarra/bookhaven/security/Config.java
@@ -1,5 +1,6 @@
 package giuliomarra.bookhaven.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -10,18 +11,40 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class Config {
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.formLogin(http -> http.disable());
         httpSecurity.csrf(http -> http.disable());
         httpSecurity.sessionManagement(http -> http.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         httpSecurity.cors(Customizer.withDefaults());
-        httpSecurity.authorizeHttpRequests(http -> http.requestMatchers("/**").permitAll());
+
+
+        httpSecurity.authorizeHttpRequests(http -> http
+                .requestMatchers("/auth/**").permitAll()
+                .requestMatchers("/api/public/**").permitAll()
+
+                .requestMatchers("/api/users/create").hasAuthority("STAFF")
+                .requestMatchers("/api/admin/**").hasAuthority("STAFF")
+                .requestMatchers("/api/staff/**").hasAuthority("STAFF")
+
+                .requestMatchers("/api/users/profile").hasAnyAuthority("USER", "STAFF")
+                .requestMatchers("/api/books/**").hasAnyAuthority("USER", "STAFF")
+
+                .anyRequest().authenticated()
+        );
+
+        httpSecurity.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
         return httpSecurity.build();
     }
 
