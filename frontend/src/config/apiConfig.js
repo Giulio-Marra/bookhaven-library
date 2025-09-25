225b98cbb1039cca65fcb833c33e98648d5f28ad
Giulio-Marra <giuliomarramail@gmail.com>
2025-09-25 09:41:06 +0200

    Update apiConfig.js

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "http://192.168.1.57:3001";
+export const API_BASE_URL = "https://bookhaven-library.onrender.com";
 // http://192.168.1.57:3001
 // https://bookhaven-library.onrender.com
